153fdc95c6fbeaabd5ea9a4edcb601c0b22ea8a0
Andy Macdonald <hello@andymacdonald.dev>
2022-09-15 19:24:29 +0100

    Add some boilerplate components

--- website/src/App.js
+++ website/src/App.js
@@ -1,10 +1,13 @@
 
+import Header from './components/header/Header';
+import Hero from './components/sections/Hero';
 import './index.css';
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='h-full w-full'>
+      <Header />
+      <Hero />
+    </div>
   )
 }
