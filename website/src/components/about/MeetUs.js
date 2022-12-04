bf94d4bfd92b3af24a9b5046a22f9842537d9f80
Andy Macdonald <hello@andymacdonald.dev>
2022-12-04 20:16:35 +0000

    Fix images

--- website/src/components/about/MeetUs.js
+++ website/src/components/about/MeetUs.js
@@ -6,7 +6,7 @@ const people = [
         favouriteMovie: "Arctic Monkeys, Rolling Stones & AC/DC",
         favouriteQuote: "You do not rise to the level of your goals, you fall to the level of your systems.",
         imageUrl:
-            'https://media-exp1.licdn.com/dms/image/C4E03AQHNlR25wxGVkQ/profile-displayphoto-shrink_800_800/0/1651934981132?e=1668643200&v=beta&t=tMAEYU1wvCv1llYZOqPcpTLQzU6QFgl0w6JWCJHrtgk',
+            '/images/alistair.png',
     },
     {
         name: 'Andy Macdonald',
@@ -14,7 +14,7 @@ const people = [
         favouriteMovie: "Back to the Future",
         favouriteQuote: "It is better to fail in originality than to succeed in imitation.",
         imageUrl:
-            'https://media-exp1.licdn.com/dms/image/C5603AQFNqy8ePdcVsg/profile-displayphoto-shrink_800_800/0/1573405670049?e=1668643200&v=beta&t=hv0-WtRz3x3iVxCIj3pQAkXvQiwONxE4QFjG3MEUkPY',
+            '/images/andy.jpeg',
     },
 
 ]
@@ -35,7 +35,7 @@ export default function MeetUs() {
                             <li key={person.name}>
                                 <div className="space-y-4 sm:grid sm:grid-cols-3 sm:gap-6 sm:space-y-0 lg:gap-8 ">
                                     <div className="aspect-w-3 aspect-h-2 h-0 sm:aspect-w-3 sm:aspect-h-4">
-                                        <img className="transition-all rounded-lg hover:scale-90 object-cover shadow-lg" src={person.imageUrl} alt="" />
+                                        <img className="transition-all rounded-lg hover:scale-90 object-cover shadow-lg grayscale" src={person.imageUrl} alt="" />
                                     </div>
                                     <div className="sm:col-span-2 ml-4">
                                         <div className="space-y-4">
