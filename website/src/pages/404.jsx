e18bb19696afa8c433d67360fa5a71a17168da67
Alistair Hey <alistair@heyal.co.uk>
2024-01-04 15:41:08 +0000

    Add success page
    Signed-off-by: Alistair Hey <alistair@heyal.co.uk>

--- website/src/pages/404.jsx
+++ website/src/pages/404.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Error () {
     return (
         <>
-            <div className="grid min-h-full grid-cols-1 grid-rows-[1fr,auto,1fr] bg-white lg:grid-cols-[max(50%,36rem),1fr]">
+            <div className="grid min-h-full grid-cols-1 grid-rows-[1fr,auto,1fr] lg:grid-cols-[max(50%,36rem),1fr]">
                 <header className="mx-auto w-full max-w-7xl px-6 pt-6 sm:pt-10 lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:px-8">
                     <Link href="/">
                         <span className="sr-only">Your Company</span>
